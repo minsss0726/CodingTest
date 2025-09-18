c234f81fc41703e7a603b0976c2922d6839e2852
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 15:29:26 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 42.45 ms, Memory: 155 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    int[] answer = new int[4];
+    static int mainDot = 0;
+    static int maxDot = 0;
+    static int[] toGoing;
+    static int[] inComing;
+ 
+    
+    public void initGraph(int[][] edges){
+        
+        for (int[] edge: edges){
+            maxDot = Math.max(maxDot,Math.max(edge[0],edge[1]));
+        }
+        
+        inComing = new int[maxDot+1];
+        toGoing = new int[maxDot+1];
+        
+        for (int i = 0 ; i < edges.length ; i++) {
+            toGoing[edges[i][0]]++;
+            inComing[edges[i][1]]++;
+        }
+        
+        for (int i = 1 ; i <= maxDot ; i++){
+            if (inComing[i] == 0 && toGoing[i] >= 2) {
+                mainDot = i;
+                answer[0] = i;
+                break;
+            }
+        }
+        
+    }
+    
+    public void findGraph(int[][] edges){
+        for (int i = 1; i <= maxDot ; i++ ){
+            if ( i != mainDot && toGoing[i] == 0 && inComing[i] >= 1){
+                answer[2] ++;
+            }
+        }
+        
+        for (int i = 1; i <= maxDot ; i++ ){
+            if ( i != mainDot && toGoing[i] >= 2 && inComing[i] >= 2)
+                answer[3]++;
+        }
+        
+        if ( toGoing[mainDot] > answer[2] + answer[3] ){
+            answer[1] = toGoing[mainDot] - answer[2] - answer[3];
+        }
+        
+    }
+    
+    
+    public int[] solution(int[][] edges) {
+        
+        initGraph(edges);
+        findGraph(edges);
+        
+        return answer;
+    }
+}
