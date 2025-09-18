880cbbf1077b14575d9361aec91d6ef5e3545cfc
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 15:38:47 +0900

    [level 2] Title: 모음 사전, Time: 7.25 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,42 @@
+class Solution {
+    char[] docs = new char[]{'A','E','I','O','U'};
+
+    public int solution(String word) {
+        int answer = 1;
+        String now = "A";
+        
+        MainLoop: while (!word.equals(now)){
+            answer++;
+            // System.out.println(now);
+            char first_char = now.charAt(0);
+            if (now.charAt(0) == first_char && now.length() < 5){
+                now += "A";
+                continue MainLoop;
+            }
+            
+            while (now.charAt(now.length()-1) == 'U' && now.length() > 1){
+                now = now.substring(0,now.length()-1);
+            }
+            now = goNext(now);
+            
+        }
+        
+        
+        return answer;
+    }
+    
+    public String goNext(String now){
+        if (now.charAt(now.length()-1) == 'A'){
+            now = now.substring(0,now.length()-1) + 'E';
+        } else if (now.charAt(now.length()-1) == 'E'){
+            now = now.substring(0,now.length()-1) + 'I';
+        } else if (now.charAt(now.length()-1) == 'I'){
+            now = now.substring(0,now.length()-1) + 'O';
+        } else if (now.charAt(now.length()-1) == 'O'){
+            now = now.substring(0,now.length()-1) + 'U';
+        } 
+        
+        
+        return now;
+    }
+}
