382c6293fe8fbdc69ca247fcfdf41244f111811e
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 15:32:29 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 7.58 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        
+        ArrayList<Integer> left_nums = new ArrayList<>(List.of(1,4,7,-1));
+        ArrayList<Integer> right_nums = new ArrayList<>(List.of(3,6,9,-2));
+        ArrayList<Integer> mid_nums = new ArrayList<>(List.of(2,5,8,0));
+        
+        int[] left_pos = { 1 , 3 };
+        int[] right_pos = { 3, 3 };
+        int[] mid_pos = { 2 , -1}; // [0] == 0 left , [0] == 1 right
+        
+        
+        for (int num : numbers){
+            if (right_nums.contains(num)){
+                answer += "R";
+                right_pos[0] = 3;
+                right_pos[1] = right_nums.indexOf(num);
+            } else if (left_nums.contains(num)){
+                answer += "L";
+                left_pos[0] = 1;
+                left_pos[1] = left_nums.indexOf(num);
+            } else {
+                mid_pos[1] = mid_nums.indexOf(num);
+                int left_dis = (int)(Math.abs(left_pos[0] - mid_pos[0]) + Math.abs(left_pos[1] - mid_pos[1]));
+                int right_dis = (int)(Math.abs(right_pos[0] - mid_pos[0]) + Math.abs(right_pos[1] - mid_pos[1]));
+                if (left_dis == right_dis){
+                    if (hand.equals("right")){
+                        answer += "R";
+                        right_pos[0] = 2;
+                        right_pos[1] = mid_nums.indexOf(num);
+                    } else {
+                        answer += "L";
+                        left_pos[0] = 2;
+                        left_pos[1] = mid_nums.indexOf(num);
+                    }
+                } else if ( left_dis > right_dis){
+                    answer += "R";
+                    right_pos[0] = 2;
+                    right_pos[1] = mid_nums.indexOf(num);
+                } else {
+                    answer += "L";
+                    left_pos[0] = 2;
+                    left_pos[1] = mid_nums.indexOf(num);
+                }
+            }
+            //System.out.println(Arrays.toString(left_pos)+"  "+Arrays.toString(right_pos)+"  "+num +"  "+ answer);
+        }
+        
+        
+        
+        return answer;
+    }
+}
