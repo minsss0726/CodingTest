f0a369f2b0976d005457ba13f33e95358b49eff2
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 15:34:38 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 150.92 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,78 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        
+        int[][][] checkpoint = new int[][][]{ { {-1,0} , {-1,-1}, {0,-1} }, 
+                                           { {1,0 } , {1,-1}, {0,-1} },
+                                           { {-1,0 } , {-1,1}, {0,1} },
+                                           { {1,0 } , {1,1}, {0,1} } };
+        
+        char[][] newBoard = new char[board.length][board[0].length()];
+        
+        for (int i = 0; i<board.length ; i++){
+            for (int j =0 ; j<board[0].length(); j++){
+                newBoard[i][j] = board[i].charAt(j);
+            }
+        }
+        
+        MainRun: while (true) {
+        
+            HashSet<String> removeDots = new HashSet<>();
+            for (int i = 1; i < m-1 ; i++){
+                Run1 : for (int j = 1; j < n-1 ; j++){
+                    char ch = newBoard[i][j];
+                    if (ch =='X'){
+                        continue Run1;
+                    }
+                    for(int z = 0 ; z < checkpoint.length ; z++){
+                        int count = 0;
+                        ArrayList<int[]> dots = new ArrayList<>();
+                        dots.add(new int[]{i,j});
+                        for (int x = 0 ; x < checkpoint[z].length ; x++){
+                            if ( ch == newBoard[i - checkpoint[z][x][0]][j-checkpoint[z][x][1]]){
+                                count++;
+                                dots.add(new int[]{i - checkpoint[z][x][0],j-checkpoint[z][x][1]});
+                            }
+                        }
+                        if (count == 3) {
+                            for (int y = 0; y < dots.size() ; y++){
+                                removeDots.add( String.valueOf(dots.get(y)[0])+","+String.valueOf(dots.get(y)[1]) );
+                            }
+                        }
+                    }
+                }
+            }
+            
+            answer += removeDots.size();
+            if (removeDots.size() == 0){
+                break MainRun;
+            }  
+            
+            
+            for (String removeDot:removeDots){
+                String[] dot = removeDot.split(",");
+                newBoard[Integer.parseInt(dot[0])][Integer.parseInt(dot[1])] = 'X';
+            }
+
+            for ( int i = 0 ; i < newBoard[0].length ; i++){
+                LinkedList<Character> list = new LinkedList<>();
+                for (int j = newBoard.length - 1; j >=0 ; j-- ){
+                    if (newBoard[j][i] != 'X'){
+                        list.add(newBoard[j][i]);
+                    }
+                }
+                for (int j = newBoard.length - 1; j >=0 ; j-- ){
+                    if ( list.size() > 0 ){
+                        newBoard[j][i] = (char)list.poll();
+                    } else {
+                        newBoard[j][i] = 'X';
+                    }
+                }
+            }
+            
+        }
+        
+        return answer;
+    }
+}
