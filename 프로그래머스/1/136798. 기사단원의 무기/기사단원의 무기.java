ccea54483377887efe31a5664be475380eaeb3ab
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 14:22:25 +0900

    [level 1] Title: 기사단원의 무기, Time: 8095.45 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] arr = new int[number];
+        arr[0] = 1;
+        
+        for (int i = 1 ; i < number ; i++){
+            arr[i] = countMeasure(i+1);
+        }
+        // System.out.println(Arrays.toString(arr));
+        for (int measure:arr){
+            if (measure > limit){
+                answer += power;
+            } else {
+                answer += measure;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int countMeasure(int number){
+        Set<Integer> list = new HashSet<>();
+        for (int i = 1; i <= number / 2; i++){
+            if (number % i == 0){
+                list.add(i);
+                list.add(number/i);
+            }
+        }
+        return list.size();
+    }
+}
