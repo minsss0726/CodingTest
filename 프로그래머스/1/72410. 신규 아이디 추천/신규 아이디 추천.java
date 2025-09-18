cf78c1a1f6c6b5552dc36279ac0d26c3ecb67e39
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 15:32:13 +0900

    [level 1] Title: 신규 아이디 추천, Time: 10.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,66 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        
+        String[] special_words = {"~","!","@","#","$","%","^","&"
+                                 ,"*","(",")","=","+","[","{","]","}",":"
+                                ,"?",",","<",">","/"};
+        
+        String dots = "..";
+        
+        new_id = new_id.toLowerCase();
+        
+        System.out.println(new_id);
+        
+        for (String special_word:special_words){
+            new_id = new_id.replace(special_word,"");
+        }
+        System.out.println(new_id);
+        
+        for (int i = 0; i < new_id.length(); i++){
+            new_id = new_id.replace(dots,".");
+        }
+        System.out.println(new_id);
+        
+        if ( new_id.startsWith(".") ){
+            new_id = new_id.substring(1,new_id.length());
+        }
+        if ( new_id.endsWith(".") ){
+            new_id = new_id.substring(0,new_id.length()-1);
+        }
+        System.out.println(new_id);
+        
+        new_id = new_id.replace(" ","a");
+        if (new_id.length() == 0){
+            new_id = "a";
+        }
+        System.out.println(new_id);
+        
+        if (new_id.length() >= 16){
+            new_id = new_id.substring(0,15);
+        }
+        System.out.println(new_id);
+        
+        if ( new_id.startsWith(".") ){
+            new_id = new_id.substring(1,new_id.length());
+        }
+        if ( new_id.endsWith(".") ){
+            new_id = new_id.substring(0,new_id.length()-1);
+        }
+        System.out.println(new_id);
+        
+        new_id = new_id.replace(" ","a");
+        if (new_id.length() == 0){
+            new_id = "a";
+        }
+        
+        while ((new_id.length() < 3) && (new_id.length() >= 1)){
+            new_id = new_id + String.valueOf(new_id.charAt(new_id.length()-1));
+        }
+        System.out.println(new_id);
+        
+        answer = new_id;
+        
+        return answer;
+    }
+}
