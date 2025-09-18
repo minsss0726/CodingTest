a05a77dae7f9e9d04cdd15eeb41abc3f7144e406
minsss0726 <wndtmgld5551@gmail.com>
2025-09-18 15:29:14 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 122.76 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -0,0 +1,21 @@
+class Solution {
+     public int solution(int n, long l, long r) {
+        int answer = 0;
+
+        for (l--; l < r; l++) {
+            if (check(l)) {
+                //System.out.println(l);
+                answer++;
+            }   
+        }
+
+        return answer;
+    }
+
+    boolean check(long l) {
+        if (l < 5 && l != 2) return true;
+        if ((l - 2) % 5 == 0) return false;
+
+        return check(l / 5);
+    }
+}
