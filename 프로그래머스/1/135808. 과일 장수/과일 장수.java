58421fc057687c60f35bebf02bb87e007396b889
minsss0726 <wndtmgld5551@gmail.com>
2025-10-15 21:21:26 +0900

    [level 1] Title: 과일 장수, Time: 73.82 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        
+        for (int i = score.length-1; i >= score.length % m; i -= m){
+            answer += (score[i-m+1] * m);
+        }
+        
+        return answer;
+    }
+}
+    
