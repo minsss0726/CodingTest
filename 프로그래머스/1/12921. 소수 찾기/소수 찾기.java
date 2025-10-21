433dbc321951586aac7e0aceb38f158a8b6ceade
minsss0726 <wndtmgld5551@gmail.com>
2025-10-21 14:52:13 +0900

    [level 1] Title: 소수 찾기, Time: 27.98 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        for(boolean num:isPrime){
+            if(num) answer++;
+        }
+        
+        return answer;
+    }
+}
