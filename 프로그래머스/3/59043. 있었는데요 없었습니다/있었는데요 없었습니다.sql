45b43e4c8cf147c38680adad39e00bd5aec7493b
minsss0726 <wndtmgld5551@gmail.com>
2026-02-20 14:46:52 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT ai.animal_id as animal_id, ai.name as name
+from animal_ins as ai
+join animal_outs as ao
+on ai.animal_id = ao.animal_id
+where ai.datetime > ao.datetime
+order by ai.datetime asc;
